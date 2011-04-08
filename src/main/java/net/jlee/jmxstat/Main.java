69b740be57105ffd0c9c65a40bf4930bf79a673d
bdelbosc <bdelbosc@nuxeo.com>
2011-04-08 10:19:04 +0200

    Format code, add license header

--- src/main/java/net/jlee/jmxstat/Main.java
+++ src/main/java/net/jlee/jmxstat/Main.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2011 Jonathan Lee
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *   Jonathan Lee
+ *   Benoit Delbosc
+ *
+ */
 package net.jlee.jmxstat;
 
 import java.io.IOException;
@@ -13,15 +33,25 @@ import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 
 public class Main {
-    
+
     public static int MAX_RETRY = 5;
-    
+
     private static class MbeanAttr {
         ObjectName name;
+
         String attr;
+
         String subAttr;
-        public MbeanAttr(ObjectName name, String attr) { this(name, attr, null); }
-        public MbeanAttr(ObjectName name, String attr, String subAttr) { this.name = name; this.attr = attr; this.subAttr = subAttr; }
+
+        public MbeanAttr(ObjectName name, String attr) {
+            this(name, attr, null);
+        }
+
+        public MbeanAttr(ObjectName name, String attr, String subAttr) {
+            this.name = name;
+            this.attr = attr;
+            this.subAttr = subAttr;
+        }
     }
 
     private static void error(String msg, int err) {
@@ -30,25 +60,29 @@ public class Main {
     }
 
     private static void usage() {
-        error("Usage:\n" +
-              "   java -jar jmxstat.jar <host:port> [mbean.name[attribute.field], ...] [interval]", 
-              1);
+        error("Usage:\n" //
+                + "java -jar jmxstat.jar <host:port> "
+                + "[mbean.name[attribute.field], ...] [interval]", 1);
     }
 
-    // Parse the list of mbean attributes from the command line (arguments 2 and on)   
-    private static List<MbeanAttr> parseMbeans(String[] args) throws MalformedObjectNameException, NullPointerException {
+    // Parse the list of mbean attributes from the command line (arguments 2 and
+    // on)
+    private static List<MbeanAttr> parseMbeans(String[] args)
+            throws MalformedObjectNameException, NullPointerException {
         List<MbeanAttr> attrList = new ArrayList<MbeanAttr>();
         for (int i = 1; i < args.length; i++) {
             String def = args[i];
             int startIdx = def.indexOf("[");
             int endIdx = def.lastIndexOf("]");
             if (startIdx >= 0 && endIdx > startIdx) {
-                ObjectName objectName = new ObjectName(def.substring(0, startIdx));
-                String[] attrs = def.substring(startIdx+1, endIdx).split(",");
+                ObjectName objectName = new ObjectName(def.substring(0,
+                        startIdx));
+                String[] attrs = def.substring(startIdx + 1, endIdx).split(",");
                 for (String attr : attrs) {
                     int subIdx = attr.indexOf(".");
                     if (subIdx >= 0) {
-                        attrList.add(new MbeanAttr(objectName, attr.substring(0, subIdx), attr.substring(subIdx+1)));
+                        attrList.add(new MbeanAttr(objectName, attr.substring(
+                                0, subIdx), attr.substring(subIdx + 1)));
                     } else {
                         attrList.add(new MbeanAttr(objectName, attr));
                     }
@@ -57,9 +91,10 @@ public class Main {
         }
         return attrList;
     }
-    
+
     private static MBeanServerConnection connect(String url) throws IOException {
-        JMXServiceURL jmxUrl = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://" + url + "/jmxrmi");
+        JMXServiceURL jmxUrl = new JMXServiceURL(
+                "service:jmx:rmi:///jndi/rmi://" + url + "/jmxrmi");
         JMXConnector jmxc = JMXConnectorFactory.connect(jmxUrl, null);
         return jmxc.getMBeanServerConnection();
     }
@@ -70,52 +105,61 @@ public class Main {
         int retry = 0;
         int refresh = 5;
         List<MbeanAttr> attrs = null;
-        
+
         // Parse arguments
-        if (args.length < 1) { usage(); }
+        if (args.length < 1) {
+            usage();
+        }
         String url = args[0];
         try {
-            refresh = Integer.parseInt(args[args.length-1]);
-        } catch (Exception e) { /* nop */ }
+            refresh = Integer.parseInt(args[args.length - 1]);
+        } catch (Exception e) { /* nop */
+        }
         try {
             attrs = parseMbeans(args);
         } catch (Exception e) {
             error("Invalid mbean attribute list - " + e.toString(), 1);
         }
-        
+
         for (;;) {
             try {
                 // Connect to JMX server
                 MBeanServerConnection mbsc = connect(url);
-                retryEnabled = true;  // only allow retries after first successful connection
-            
+                retryEnabled = true; // only allow retries after first
+                // successful connection
+
                 for (;;) {
-                    StringBuilder out = new StringBuilder(); 
+                    StringBuilder out = new StringBuilder();
 
                     // Read each mbean attribute specified on the command line
                     for (MbeanAttr attr : attrs) {
                         out.append(attr.attr);
                         Object val = mbsc.getAttribute(attr.name, attr.attr);
 
-                        // Read fields from CompositeData attributes if user specified a sub-attribute 
-                        // via dot notation (e.g. java.lang:type=Memory[HeapMemoryUsage.max])
-                        if (attr.subAttr != null && val instanceof CompositeData) {
+                        // Read fields from CompositeData attributes if user
+                        // specified a sub-attribute
+                        // via dot notation (e.g.
+                        // java.lang:type=Memory[HeapMemoryUsage.max])
+                        if (attr.subAttr != null
+                                && val instanceof CompositeData) {
                             out.append(".").append(attr.subAttr);
                             val = ((CompositeData) val).get(attr.subAttr);
                         }
-                        
+
                         out.append("=").append(val).append(" ");
                     }
-                    
+
                     System.out.println(out.toString());
                     Thread.sleep(refresh * 1000);
-                    retry = 0;  // reset exponential retry back-off after a successful read 
+                    retry = 0; // reset exponential retry back-off after a
+                    // successful read
                 }
             } catch (IOException e) {
                 if (retryEnabled && retry < MAX_RETRY) {
                     // Exponential retry back-off
                     int sleep = Math.min(30, (int) Math.pow(2, ++retry));
-                    System.err.println("Communcation error. Retry [" + retry + "/" + MAX_RETRY + "] in " + sleep + "s.\n" + e);
+                    System.err.println("Communcation error. Retry [" + retry
+                            + "/" + MAX_RETRY + "] in " + sleep + "s.\n" + e);
                     Thread.sleep(sleep * 1000);
                 } else {
                     error("Error reading from " + url + "\n" + e, 2);
